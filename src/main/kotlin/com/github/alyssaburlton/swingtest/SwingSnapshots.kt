6b9d279e3ee018a74d0d088e40e5ffed3b828b4d
Alyssa <alexburlton@hotmail.com>
2023-04-17 13:17:07 +0100

    Allow wiring of tolerance params

--- src/main/kotlin/com/github/alyssaburlton/swingtest/SwingSnapshots.kt
+++ src/main/kotlin/com/github/alyssaburlton/swingtest/SwingSnapshots.kt
@@ -1,7 +1,6 @@
 package com.github.alyssaburlton.swingtest
 
 import com.github.romankh3.image.comparison.ImageComparison
-import com.github.romankh3.image.comparison.ImageComparisonUtil
 import com.github.romankh3.image.comparison.model.ImageComparisonState
 import io.kotest.assertions.fail
 import io.kotest.matchers.shouldBe
@@ -18,12 +17,10 @@ const val ENV_SCREENSHOT_OS = "screenshotOs"
 private const val DEFAULT_WIDTH = 200
 private const val DEFAULT_HEIGHT = 200
 
-fun Icon.shouldMatch(other: Icon)
-{
+fun Icon.shouldMatch(other: Icon) {
     toBufferedImage().isEqual(other.toBufferedImage()) shouldBe true
 }
-private fun Icon.toBufferedImage(): BufferedImage
-{
+private fun Icon.toBufferedImage(): BufferedImage {
     val bi = BufferedImage(iconWidth, iconHeight, BufferedImage.TYPE_INT_RGB)
     val g = bi.createGraphics()
     paintIcon(null, g, 0, 0)
@@ -34,7 +31,7 @@ private fun Icon.toBufferedImage(): BufferedImage
 fun JComponent.toBufferedImage(): BufferedImage {
     val width = getWidthForSnapshot()
     val height = getHeightForSnapshot()
-    
+
     val img = BufferedImage(width, height, BufferedImage.TYPE_4BYTE_ABGR)
     val g2 = img.createGraphics()
     paint(g2)
@@ -53,13 +50,16 @@ private fun JComponent.getHeightForSnapshot(): Int = when {
     else -> DEFAULT_HEIGHT
 }
 
-fun JComponent.shouldMatchImage(imageName: String) {
+@JvmOverloads
+fun JComponent.shouldMatchImage(imageName: String, pixelTolerance: Double = 0.1, failingPixelsThreshold: Double = 0.0) {
     verifyOs()
 
     val overwrite = System.getProperty(ENV_UPDATE_SNAPSHOT) == "true"
     val img = toBufferedImage()
 
-    val callingSite = Throwable().stackTrace[1].className
+    val stackFrames = Throwable().stackTrace.toList()
+
+    val callingSite = stackFrames.first { it.className != "com.github.alyssaburlton.swingtest.SwingSnapshotsKt" }.className
     val imgPath = "src/test/resources/__snapshots__/$callingSite"
 
     val file = File("$imgPath/$imageName.png")
@@ -73,7 +73,10 @@ fun JComponent.shouldMatchImage(imageName: String) {
         ImageIO.write(img, "png", file)
     } else {
         val savedImg = ImageIO.read(file)
-        val result = ImageComparison(savedImg, img).compareImages()
+        val result = ImageComparison(savedImg, img)
+            .setPixelToleranceLevel(pixelTolerance)
+            .setAllowingPercentOfDifferentPixels(failingPixelsThreshold)
+            .compareImages()
 
         if (result.imageComparisonState != ImageComparisonState.MATCH) {
             val failedFile = File("$imgPath/$imageName.failed.png")
@@ -92,8 +95,9 @@ private fun verifyOs() {
 
     val os = System.getProperty("os.name").lowercase()
     if (osForScreenshots.isNotEmpty()) {
-        Assumptions.assumeTrue(os.contains(osForScreenshots),
-            "Wrong OS for screenshot tests (wanted $osForScreenshots, found $os)"
+        Assumptions.assumeTrue(
+            os.contains(osForScreenshots),
+            "Wrong OS for screenshot tests (wanted $osForScreenshots, found $os)",
         )
     }
 }
